dc300eeaf13e46d3fa8238c7d5e6c2ed0583c388
WonyuChoi <phillip07290@naver.com>
2024-01-09 19:58:53 +0900

    feat: 트랜잭션, 엔티티 매니저 fixture

---
+++ src/main/java/fixture/TransactionFixture.java
@@ -0,0 +1,43 @@
+package fixture;
+
+import java.util.function.Consumer;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class TransactionFixture {
+
+    private final static EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+    private final static EntityManager em = emf.createEntityManager();
+    private final static EntityTransaction tx = em.getTransaction();
+
+    public static void useEntityManager(Consumer<EntityManager> emConsumer) {
+        try {
+            tx.begin();
+
+            emConsumer.accept(em);
+
+            tx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+
+    public static void useTransaction(Consumer<EntityTransaction> txConsumer) {
+        try {
+            txConsumer.accept(tx);
+        } catch (Exception e) {
+            e.printStackTrace();
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+
+}
